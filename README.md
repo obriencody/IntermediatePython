dbf8e4a2c0206c76c32e1d71d85c238cecbcac00
t24 <codyobrien@mail.weber.edu>
2021-08-09 09:42:14 -0600

    new functions, comments, note-taking

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # IntermediatePython
 learning new python language
+
+## Instructor project
+https://github.com/hugo-wsu/python-hafb
+
+### Local Installations
+
+Login to system using:
+- Username: **txx** (xx is terminal number)
+- Password: **Hello123**
+
+- Need to open git bash and run the following command
+'''bash
+pip install pytest
+'''
+Reload 'Pycharm'
--- hello.py
+++ hello.py
@@ -1,1 +1,35 @@
-print("Hello Waldo!")
+#!/usr/bin/env python3
+"""
+Author: Cody O'Brien
+Purpose: Say "Hello to Waldo"
+"""
+
+# Purpose: Say "Hello to Waldo"
+import argparse
+
+
+def get_args():
+    """
+    Get the command line arguments
+    :return: list of parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name is greet')
+    return parser.parse_args()
+
+
+def main():
+    """
+    This is where the action is
+    :return: N/A
+    """
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == "__main__":
+    main()
+# print(__name__)
+
+# print("Hello Waldo!")
