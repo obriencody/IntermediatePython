dffee0e4d39fb374bb1a2196e2552753f393a4f3
t24 <codyobrien@mail.weber.edu>
2021-08-10 15:46:14 -0600

    support contains

--- Day2/sorted_set.py
+++ Day2/sorted_set.py
@@ -9,7 +9,10 @@ Purpose: Sorted Set Class
 class SortedSet:
     def __init__(self, items=None):
 
-        self._items = sorted(items)
+        self._items = sorted(items) if items is not None else []
+
+    def __contains__(self, item):
+        return item in self._items
 
 
 # --------------------------------------------------
