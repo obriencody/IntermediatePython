fd7700033081d435b708bacc0cca87fb01b30c1b
t24 <codyobrien@mail.weber.edu>
2021-08-10 15:56:07 -0600

    support contains

--- Day2/sorted_set.py
+++ Day2/sorted_set.py
@@ -9,11 +9,14 @@ Purpose: Sorted Set Class
 class SortedSet:
     def __init__(self, items=None):
 
-        self._items = sorted(items) if items is not None else []
+        self._items = sorted(set(items)) if items is not None else []
 
     def __contains__(self, item):
         return item in self._items
 
+    def __len__(self):
+        """Length Protocol"""
+        return len(self._items)
 
 # --------------------------------------------------
 def main():
